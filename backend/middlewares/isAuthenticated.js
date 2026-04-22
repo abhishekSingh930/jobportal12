ba37d5cb11bdecd11e4b59b28b10340e2ed42a5f
Abhishek Singh <sabhishek34191@gmail.com>
2026-04-22 08:18:20 +0530

    fix: resolved pending request bug by correcting JWT secret key and adding catch block responses

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -9,7 +9,7 @@ const isAuthenticated = async (req, res, next) => {
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        const decode = await jwt.verify(token, process.env.JWT_SECRET);
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
@@ -19,7 +19,12 @@ const isAuthenticated = async (req, res, next) => {
         req.id = decode.userId;
         next();
     } catch (error) {
+
         console.log(error);
+        return res.status(500).json({
+            message: "Internal server error or token expired",
+            success: false
+        })
     }
 }
 export default isAuthenticated;
